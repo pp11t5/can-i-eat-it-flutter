319db9281d414eb5bdf36a90f3c1fd26b5138f9f
WAcAW9 <wacaw9@gmail.com>
2026-08-04 12:07:59 +0900

    chore: HealthProfileController Riverpod 생성 해시 동기화

--- lib/features/health_profile/data/health_profile_providers.g.dart
+++ lib/features/health_profile/data/health_profile_providers.g.dart
@@ -81,7 +81,7 @@ final onboardedStatusProvider = AutoDisposeFutureProvider<bool>.internal(
 // ignore: unused_element
 typedef OnboardedStatusRef = AutoDisposeFutureProviderRef<bool>;
 String _$healthProfileControllerHash() =>
-    r'c9f3c03d82371210c5c980f538ff1a249ba7c445';
+    r'6a33bc45677410f2803c61a8d3d180009ff54467';
 
 /// 건강 프로필 상태 컨트롤러 (AsyncNotifier).
 ///
